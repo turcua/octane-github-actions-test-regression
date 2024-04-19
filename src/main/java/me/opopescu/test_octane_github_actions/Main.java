3bb1ecd8a73e16bbda10504ffd949b404ad5e8c0
Ovidiu Stefan Popescu <opopescu@opentext.com>
2024-04-19 14:26:15 +0300

    Update

--- src/main/java/me/opopescu/test_octane_github_actions/Main.java
+++ src/main/java/me/opopescu/test_octane_github_actions/Main.java
@@ -3,6 +3,6 @@ package me.opopescu.test_octane_github_actions;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Updating application...");
-        System.out.println("Hello world!");
+        System.out.println("Hello world! ");
     }
 }
