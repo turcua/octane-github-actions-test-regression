13a278ccf77cb6d568dcb7e6a45919125cce8d12
Ovidiu Stefan Popescu <opopescu@opentext.com>
2024-05-08 15:44:28 +0300

    dist

--- dist/index.js
+++ dist/index.js
@@ -72337,11 +72337,11 @@ const handleEvent = (event) => __awaiter(void 0, void 0, void 0, function* () {
                     number: (runNumber === null || runNumber === void 0 ? void 0 : runNumber.toString()) || pipelineData.buildCiId,
                     skipValidation: true
                 };
-                (0, utils_1.sleep)(10000);
+                yield (0, utils_1.sleep)(10000);
                 console.log(`Create CHILD pipeline: ${JSON.stringify(ciJobEvent)}`);
                 yield octaneClient_1.default.sendEvents([ciJobEvent], pipelineData.instanceId, pipelineData.baseUrl);
                 pipelineData = yield (0, pipelineDataService_1.getPipelineData)(event, false, false);
-                (0, utils_1.sleep)(5000);
+                yield (0, utils_1.sleep)(5000);
             }
             const rootParentCauseData = {
                 isRoot: true,
