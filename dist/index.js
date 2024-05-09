5c1ba52754280700e336888ab413523503a521a3
Ovidiu Stefan POPESCU <opopescu@opentext.com>
2024-05-09 12:23:25 +0300

    dist

--- dist/index.js
+++ dist/index.js
@@ -72596,20 +72596,21 @@ const pollForJobsOfTypeToFinish = (owner, repoName, currentRun, workflowRunId, s
         const notFinishedRuns = yield getNotFinishedRuns(owner, repoName, startTime, currentRun);
         // Integration job name structure is: OctaneIntegration#${{github.event.action}}#${{github.event.workflow_run.id}}
         console.log(`Before getting not finished runs`);
-        const runsToWaitFor = yield Promise.all(notFinishedRuns.filter((run) => __awaiter(void 0, void 0, void 0, function* () {
-            const jobs = (yield githubClient_1.default.getWorkflowRunJobs(owner, repoName, run.id)).filter(job => {
+        const runsMappedToTheirJobs = yield Promise.all(notFinishedRuns.map(run => githubClient_1.default.getWorkflowRunJobs(owner, repoName, run.id)));
+        const runsToWaitFor = runsMappedToTheirJobs.filter(jobsForRun => {
+            const jobs = jobsForRun.filter(job => {
                 const nameComponents = job.name.split('#');
                 const runEventType = nameComponents[1];
                 const triggeredByRunId = nameComponents[2];
-                console.log(`Found job: ${JSON.stringify(run.id)} triggered by: ${triggeredByRunId}, result: ${runEventType === eventType &&
+                console.log(`triggered by: ${triggeredByRunId}, result: ${runEventType === eventType &&
                     Number.parseInt(triggeredByRunId) === workflowRunId}`);
                 return (runEventType === eventType &&
                     Number.parseInt(triggeredByRunId) === workflowRunId);
             });
             console.log(`Current run: ${JSON.stringify(currentRun.id)}, current workflow: ${JSON.stringify(workflowRunId)} and found runs: ${JSON.stringify(jobs.map(job => job.id))}, jobs length: ${jobs.length}, result: ${jobs.length > 0}`);
             return jobs.length > 0;
-        })));
-        console.log(`After getting not finished runs: ${JSON.stringify(runsToWaitFor.map(run => run.id))}, runs: ${runsToWaitFor.length}, result: ${runsToWaitFor.length === 0}`);
+        });
+        console.log(`After getting not finished runs: -, runs: ${runsToWaitFor.length}, result: ${runsToWaitFor.length === 0}`);
         done = runsToWaitFor.length === 0;
         yield (0, utils_1.sleep)(2000);
     }
