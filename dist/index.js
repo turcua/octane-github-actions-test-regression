916c406d0d929a8810425ae1af531f2ef1f9833d
Ovidiu Stefan POPESCU <opopescu@opentext.com>
2024-05-23 17:26:48 +0300

    dist

--- dist/index.js
+++ dist/index.js
@@ -94670,7 +94670,7 @@ const handleEvent = (event) => __awaiter(void 0, void 0, void 0, function* () {
                     eventType: "started" /* CiEventType.STARTED */,
                     startTime: startTime,
                     multiBranchType: "CHILD" /* MultiBranchType.CHILD */,
-                    parentCiId: `${pipelineData.rootJobName}`,
+                    parentCiId: `${jobCiIdPrefix}/${pipelineData.rootJobName}`,
                     branch: branchName,
                     number: (runNumber === null || runNumber === void 0 ? void 0 : runNumber.toString()) || pipelineData.buildCiId,
                     skipValidation: true
