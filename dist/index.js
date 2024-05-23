16225fd1756e65ffa8e3398d2ef298e7cd63e9e4
Ovidiu Stefan POPESCU <opopescu@opentext.com>
2024-05-23 15:36:18 +0300

    dist

--- dist/index.js
+++ dist/index.js
@@ -95014,7 +95014,7 @@ const mapPipelineComponentToCiEvent = (pipelineComponent, parentComponentData, b
             ? "finished" /* CiEventType.FINISHED */
             : "started" /* CiEventType.STARTED */,
         number: (runNumber === null || runNumber === void 0 ? void 0 : runNumber.toString()) || buildCiId,
-        project: `${componentFullName}`,
+        project: `TEST/${componentFullName}`,
         projectDisplayName: componentName,
         startTime: pipelineComponent.started_at
             ? new Date(pipelineComponent.started_at).getTime()
